337cc8f0d60c2b2973f7a970c0ae6d70105a3740
Marcus Quintela <marcus.quintela@gmail.com>
2021-07-22 16:21:37 -0300

    first commit

---
+++ src/main/java/br/com/rapha/testeMundo/TesteMundoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rapha.testeMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rapha/testeMundo/TesteMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rapha.testeMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
